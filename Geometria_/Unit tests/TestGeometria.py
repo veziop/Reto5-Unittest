33da5b58f6d23567540649238bd17f22a2faf85a
Valentin <valenp97@gmail.com>
2021-11-02 16:00:14 +0100

    Comienzos del unit test

--- Geometria_/Unit tests/TestGeometria.py
+++ Geometria_/Unit tests/TestGeometria.py
@@ -1,4 +1,17 @@
 import unittest
+from Geometria_.model.Geometria import Geometria
 
-class TestGeometria:
-    pass
+
+class TestGeometria(unittest.TestCase):
+
+    def setUp(self):
+        self.shape = Geometria(10, 50, 4)
+
+    def test_geometria(self):
+        self.assertIsNotNone(self.shape)
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
