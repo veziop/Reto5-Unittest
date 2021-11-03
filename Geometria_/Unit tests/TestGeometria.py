a93723bdf7491ce6825aab5e62c0556b283fb33b
Valentin <valenp97@gmail.com>
2021-11-03 10:51:28 +0100

    Modificado la prueba de geometria para que pruebe con diez objetos Geometria con valores a b y c aleatorios entre 2-15

--- Geometria_/Unit tests/TestGeometria.py
+++ Geometria_/Unit tests/TestGeometria.py
@@ -1,17 +1,23 @@
 import unittest
 from Geometria_.model.Geometria import Geometria
+from random import randint
+from Geometria_.view.View import View
 
 
 class TestGeometria(unittest.TestCase):
 
     def setUp(self):
-        self.shape = Geometria(10, 50, 4)
+        self.shapes = [
+            Geometria(randint(2, 15), randint(2,15), randint(2, 15))
+            for i in range(10)
+        ]
 
     def test_geometria(self):
-        # self.assertIsNotNone(self.shape)
-        for i in range(1,9):
-            with self.subTest():
-                self.assertIsNotNone(self.shape.switch(i))
+        for shape in self.shapes:
+            for i in range(1,9):
+                with self.subTest():
+                    self.assertIsNotNone(shape.switch(i))
+
 
 
 
