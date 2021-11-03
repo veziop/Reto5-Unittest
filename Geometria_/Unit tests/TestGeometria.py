9c18253598dc30db4238df787f6df7ae0c8148ec
Valentin <valenp97@gmail.com>
2021-11-03 10:40:27 +0100

    Testeo anidado de la creacion del objeto Geometria, cambiando la forma con self.switch() y calculando su area

--- Geometria_/Unit tests/TestGeometria.py
+++ Geometria_/Unit tests/TestGeometria.py
@@ -8,7 +8,12 @@ class TestGeometria(unittest.TestCase):
         self.shape = Geometria(10, 50, 4)
 
     def test_geometria(self):
-        self.assertIsNotNone(self.shape)
+        # self.assertIsNotNone(self.shape)
+        for i in range(1,9):
+            with self.subTest():
+                self.assertIsNotNone(self.shape.switch(i))
+
+
 
 
 
